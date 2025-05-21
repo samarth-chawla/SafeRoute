b8c9edd944ba61320da529a4587800ddd0c50cf2
samarth-chawla <167853500+samarth-chawla@users.noreply.github.com>
2025-05-21 13:56:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: *,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
