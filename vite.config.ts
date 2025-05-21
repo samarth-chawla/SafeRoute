c2491804d524cb0c56c8f8316abf2f73cf310164
samarth-chawla <167853500+samarth-chawla@users.noreply.github.com>
2025-05-21 13:32:50 +0530

    Update vite.config.ts for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  const env = loadEnv(mode, process.cwd())
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
@@ -10,9 +11,10 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'process.env.BACKEND_URL',
         changeOrigin: true,
         secure: false,
+        rewrite: path => path.replace(/^\/api/, ''),
       },
     }
   },
